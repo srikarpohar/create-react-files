cf3843c52bb8c3b2751d468e7463b26c64b1a274
srikarpohar <srikarpohar@gmail.com>
2022-05-14 23:19:07 +0530

    Added try/catch to display error message

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
 import {createComponent} from "./commands/createComponent.js";
 import { Command } from './command-creator/command.js';
 
 async function main() {
+    try {
+        const command = new Command();
+        command.createCommand('component <component_name>', "Creates a react component with given name and options.");
+        command.createOption('-f --functional', 'Functional Component', false);
+        command.createOption('-c --class', 'Class Component', false);
+        command.createOption('-t --type [type]', `Component is 'typescript' one or 'javascript' one`, 'typescript');
+        command.createOption('-r, --redux-connect', 'Add redux connect option', false);
+        command.createOption('--cssext [cssExt]', `CSS Extension of the component's css file`, 'css');
+        command.createOption('--force', 'Force remove and replace the component file if it exists already', false);
 
-    const command = new Command();
-    command.createCommand('component <component_name>', "Creates a react component with given name and options.");
-    command.createOption('-f --functional', 'Functional Component', false);
-    command.createOption('-c --class', 'Class Component', false);
-    command.createOption('-t --type [type]', `Component is 'typescript' one or 'javascript' one`, 'typescript');
-    command.createOption('-r, --redux-connect', 'Add redux connect option', false);
-    command.createOption('--cssext [cssExt]', `CSS Extension of the component's css file`, 'css');
-    command.createOption('--force', 'Force remove and replace the component file if it exists already', false);
-
-    command.createAction(createComponent);
-    await command.parseAsync();
+        command.createAction(createComponent);
+        await command.parseAsync();
+    } catch(error) {
+        console.log(chalk.red.bold(error.message));
+    }
 }
 
 main();
